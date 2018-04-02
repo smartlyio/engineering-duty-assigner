0e8252b74cb01a0c462e97ff3c153051af5c6ecf
Oskari Virtanen <oskari@smartly.io>
2018-04-02 19:54:14 +0300

    Add ktor-jackson

--- src/main/kotlin/web/Application.kt
+++ src/main/kotlin/web/Application.kt
@@ -1,12 +1,27 @@
+package web
+
 import io.ktor.application.Application
 import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.http.ContentType
+import io.ktor.jackson.JacksonConverter
+import io.ktor.request.receive
 import io.ktor.response.respondText
 import io.ktor.routing.*
 
+data class Foo(val foo: String)
+
 fun Application.dutyAssigner() {
+    install(ContentNegotiation) {
+        register(ContentType.Application.Json, JacksonConverter())
+    }
+
     routing {
-        get("foo") {
-            call.respondText("And from separate module")
+        post("foo") {
+            val foo = call.receive<Foo>()
+            println(foo.foo)
+            call.respondText { "and from separate module" }
         }
     }
 }
